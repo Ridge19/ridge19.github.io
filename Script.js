d6f017df808304588e56b27432731a0cc0d761dd
Ridge Tagala <Riidgyy@gmail.com>
2025-02-27 16:22:07 +1100

    restart

--- Script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const themeToggle = document.getElementById("theme-toggle");
-    const body = document.body;
-
-    themeToggle.addEventListener("click", function () {
-        body.classList.toggle("light-mode");
-        themeToggle.textContent = body.classList.contains("light-mode") ? "🌙" : "☀️";
-    });
-});
